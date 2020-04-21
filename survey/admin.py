45d7096c8e8bc2e5ce027ef10d22bee953d59d8b
belendia <belendia@gmail.com>
2020-04-21 15:25:16 +0300

    Add models and make them accessable through admin interface

--- survey/admin.py
+++ survey/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from survey.models import Language, Subscriber, Survey, Message
 
-# Register your models here.
+admin.site.register(Language)
+admin.site.register(Subscriber)
+admin.site.register(Survey)
+admin.site.register(Message)
--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class CommonModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    created_by = models.ForeignKey(User, related_name='created_%(class)s',null=True, on_delete=models.SET_NULL)
+    updated_by = models.ForeignKey(User, related_name='updated_%(class)s',null=True, on_delete=models.SET_NULL)
+    class Meta:
+        abstract = True
+
+class Language(CommonModel):
+   
+    name = models.CharField(max_length=50, unique=True, null=False)
+    code = models.CharField(max_length =10, unique=True, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Subscriber(CommonModel):
+    telephone = models.CharField(max_length=20, unique=True, null=False)
+    language  = models.ForeignKey( Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.telephone
+
+class Survey(CommonModel):
+    title = models.CharField(max_length=20, null=False)
+    questions = models.TextField(null = False)
+    published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class Message(CommonModel):
+    content = models.TextField(null=False)
+
+    def __str__(self):
+        return self.content
