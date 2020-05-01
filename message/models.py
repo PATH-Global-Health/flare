a5edee4d3518557f77d2ec6f7ec8288b55bb3788
belendia <belendia@gmail.com>
2020-05-01 14:25:00 +0300

    Add new fields to message model

--- message/models.py
+++ message/models.py
@@ -3,6 +3,15 @@ from settings.models import CommonModel
 
 class Message(CommonModel):
     content = models.TextField(null=False)
+    status = models.CharField(max_length=255, null=True, blank=True)
+    completed = models.DateTimeField(null=True, blank=True)
+    celery_id = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        ordering = ('created_at',)
+
+    def __unicode__(self):
+        return self.name
 
     def __str__(self):
         return self.content
