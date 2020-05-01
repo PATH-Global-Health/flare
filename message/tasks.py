819b0dd8d159170e93c4b7bb8e6f3deabae283e0
belendia <belendia@gmail.com>
2020-05-01 20:27:59 +0300

    Integrate with AfricasTalking

--- message/tasks.py
+++ message/tasks.py
@@ -1,8 +1,10 @@
+import africastalking
 import logging
 from asgiref. sync import async_to_sync 
 from celery import shared_task
 from channels. layers import get_channel_layer
 from time import sleep
+from django.conf import settings
 from .models import Message
 
 @shared_task
@@ -15,9 +17,25 @@ def send_message(message_id):
     for lang in message.languages.all():
         print(lang.name)
     logger.info("=====================================================================")
-    # sleep(10)
-    message.status = "sent"
-    message.save()
+
+    africastalking.initialize(settings.AFRICASTALKING_USERNAME, settings.AFRICASTALKING_API_KEY)
+
+    # Initialize the SMS service
+    sms = africastalking.SMS
+
+    to = ["+251911607571"]
+    try:
+        response = sms.send(message.content, to)
+        logger.info(response)
+        
+        message.status = "sent"
+        message.save()
+    except Exception as e:
+        message.status = "error"
+        message.save()
+        logger.info('Encountered an error while sending: %s' % str(e))
+
+    
     channel_layer = get_channel_layer()
     logger.info("Sending message")
     async_to_sync(channel_layer.group_send)(
