2a6dd227ecf063543b82810c54ab2ddede5a5e40
belendia <belendia@gmail.com>
2020-05-03 21:08:18 +0300

    Change phone number into string

--- subscriber/models.py
+++ subscriber/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from settings.models import CommonModel, Language
 
 class Subscriber(CommonModel):
-    phone_number = models.IntegerField(unique=True, null=False)
+    phone_number = models.CharField(max_length = 20, unique=True, null=False)
     language  = models.ForeignKey( Language, on_delete=models.CASCADE)
 
     def __str__(self):
