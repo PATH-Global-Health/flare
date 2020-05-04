5123e905b0433db75bff9177011918541e1045a6
belendia <belendia@gmail.com>
2020-05-04 21:16:38 +0300

    Add channels signal in addition to languages for messages before starting the celery task

--- message/signals.py
+++ message/signals.py
@@ -4,13 +4,15 @@ from .models import Message
 from .tasks import send_message
 
 @receiver(m2m_changed, sender=Message.languages.through)
+@receiver(m2m_changed, sender=Message.channels.through)
 def announce_new_message(sender, **kwargs):
     if kwargs.get('action') == "post_add":
         msg = kwargs['instance']
-        # Start long running task here (using Celery)
-        send_message_task = send_message.delay(msg.id)
+        if msg.languages.all().count() >0 and msg.channels.all().count()>0:
+            # Start long running task here (using Celery)
+            send_message_task = send_message.delay(msg.id)
 
-        # Store the celery task id into the database if we wanted to
-        # do things like cancel the task in the future
-        msg.celery_id = send_message_task.id
-        msg.save()
+            # Store the celery task id into the database if we wanted to
+            # do things like cancel the task in the future
+            msg.celery_id = send_message_task.id
+            msg.save()
