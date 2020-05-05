640d3cadca576422b54094e603b1a470a10fbd52
belendia <belendia@gmail.com>
2020-05-05 16:13:26 +0300

    Set max length to none

--- survey/models.py
+++ survey/models.py
@@ -6,14 +6,14 @@ from settings.models import CommonModel
 from subscriber.models import Subscriber
 
 class OverwriteStorage(FileSystemStorage):
-    def get_available_name(self, name):
+    def get_available_name(self, name, max_length=None):
         if self.exists(name):
             os.remove(os.path.join(settings.MEDIA_ROOT, name))
         return name
    
 class Survey(CommonModel):
-    title = models.CharField(max_length=20, null=False)
-    journeys = models.FileField(upload_to='journeys', storage=OverwriteStorage())
+    title = models.CharField(max_length=200, null=False)
+    journeys = models.FileField(storage=OverwriteStorage())
 
     def __str__(self):
         return self.title
