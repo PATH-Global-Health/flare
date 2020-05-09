9f953a711ce880316665a393d7aca6d0ebd70ad0
belendia <belendia@gmail.com>
2020-05-09 13:53:20 +0300

    Extract and assign survey_id and endpoint in the serializer from the yaml file uploaded

--- survey/serializers.py
+++ survey/serializers.py
@@ -1,15 +1,23 @@
 import json
-import yaml
+import logging
 from rest_framework import serializers
 from ussd.core import UssdView
 from .models import Survey, SurveyResult
+from .helpers import read_journey, validate_ussd_journey, get_survey_endpoint_and_id
+
+logger = logging.getLogger(__name__)
 
 def validate_yaml(value):
+
     try:
-        journey = value['journeys'].read()
-        journey = yaml.full_load(journey)
+        journey = read_journey(value['journeys'])
         is_valid, errors = UssdView.validate_ussd_journey(journey)
-    except:
+
+        # validate the existance and form of a custom defined initialize_survey screen
+        if is_valid:
+            is_valid, errors = validate_ussd_journey(journey)
+    except Exception as ex:
+        logger.error(ex)
         raise serializers.ValidationError({"journeys":["The yaml file is invalid."]})
 
     if(not(is_valid)):
@@ -19,10 +27,28 @@ class SurveySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Survey
-        fields = ('id','survey_id', 'title', 'published', 'endpoint', 'journeys')#'__all__' #we need to see all the fields in language model
-        read_only_fields = ('survey_id', 'endpoint')
+        fields = ('id','survey_id', 'title', 'published', 'endpoint', 'journeys')
         validators = [ validate_yaml ]
-    
+        
+    def to_internal_value(self, data):
+        # assign endpoint and survey_id by reading from the uploaded file
+        # TODO:
+        # find a better way not to revalidate the yaml file again
+        try:
+            journey = read_journey(data['journeys'])
+            is_valid, errors = UssdView.validate_ussd_journey(journey)
+
+            if is_valid:
+                is_valid, errors = validate_ussd_journey(journey)
+            if is_valid:
+                d = get_survey_endpoint_and_id(journey)
+                data['endpoint'] = d['endpoint']
+                data['survey_id'] = d['survey_id']
+        except Exception as ex:
+            logger.error(ex)
+
+        return super(SurveySerializer,self).to_internal_value(data)
+
 
 class SurveyResultSerializer(serializers.ModelSerializer):
     class Meta:
