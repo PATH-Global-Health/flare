3bb121b40f68e9d34088c952a01c7dff554027b4
belendia <belendia@gmail.com>
2020-05-13 14:46:05 +0300

    Add static file url for Django

--- flare/urls.py
+++ flare/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 # from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('message.urls')),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('', include('gateway.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
