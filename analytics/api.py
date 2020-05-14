e3eb0c4f0c919783b34479abf40ece7199d04c36
belendia <belendia@gmail.com>
2020-05-14 22:59:06 +0300

    Correct report API

--- analytics/api.py
+++ analytics/api.py
@@ -1,8 +1,16 @@
 from rest_framework import generics
 from django.db.models import Max
+from rest_framework.response import Response
 from .models import Report
 from .serializers import ReportSerializer
 
 class ReportView(generics.RetrieveAPIView):
-    queryset = Report.objects.all().aggregate(Max('id'))
+    queryset = Report.objects.all()
     serializer_class = ReportSerializer
+    
+    def get(self, request, format=None):
+        max_record = self.get_queryset().aggregate(r=Max('pk')).get('r')
+        report = Report.objects.get(id = max_record)
+
+        serializer = ReportSerializer(report)
+        return Response(serializer.data)
