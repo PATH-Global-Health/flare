b4634dc13a8904f9a5d7cebd60aca62a77b9aad2
belendia <belendia@gmail.com>
2020-05-16 11:41:42 +0300

    Add authentication to the report api

--- analytics/api.py
+++ analytics/api.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from django.db.models import Max
 from rest_framework.response import Response
 from .models import Report
@@ -7,6 +7,9 @@ from .serializers import ReportSerializer
 class ReportView(generics.RetrieveAPIView):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
     
     def get(self, request, format=None):
         max_record = self.get_queryset().aggregate(r=Max('pk')).get('r')
