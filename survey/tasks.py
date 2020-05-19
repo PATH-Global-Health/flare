1b2071122b744cec69cdbcbde3db5c1c976122a6
belendia <belendia@gmail.com>
2020-05-19 19:54:13 +0300

    Remove firebase sync task

--- survey/tasks.py
+++ survey/tasks.py
@@ -1,7 +1,6 @@
 from celery import shared_task
 from .helpers import create_survey_result, \
                       mark_survey_result_complete, \
-                      sync_survey_result_2_firebase, \
                       copy_incomplete_data_2_survey_results, \
                       sync_survey_result_2_central_repo, \
                       check_missed_sessions_have_survey_data, \
@@ -15,9 +14,9 @@ def create_survey_result_task(survey_pk, session_key, phone_number):
 def mark_survey_result_complete_task(survey_pk, session_key, phone_number):
     mark_survey_result_complete(survey_pk, session_key, phone_number)
 
-@shared_task
-def sync_survey_result_2_firebase_task():
-    sync_survey_result_2_firebase()
+# @shared_task
+# def sync_survey_result_2_firebase_task():
+#     sync_survey_result_2_firebase()
 
 @shared_task
 def copy_incomplete_data_2_survey_results_task():
