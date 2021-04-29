1902542482d3b3e9c10eefbdc4c3e9115347be63
Belendia Serda <belendia@gmail.com>
2021-04-29 11:04:20 +0300

    Fix model reference in subscriber app model

--- apps/subscriber/models.py
+++ apps/subscriber/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from apps.settings import CommonModel, Language
+from apps.settings.models import CommonModel, Language
+
 
 class Subscriber(CommonModel):
-    phone_number = models.CharField(max_length = 20, unique=True, null=False)
-    language  = models.ForeignKey( Language, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, unique=True, null=False)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.phone_number
