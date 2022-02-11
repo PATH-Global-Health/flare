d84cdd6c1be04b820d7e1d2d5722b8aee1e2c3af
Belendia Serda <belendia@gmail.com>
2022-02-11 23:08:32 +0300

    Fix filter field

--- apps/dhis/admin.py
+++ apps/dhis/admin.py
@@ -13,7 +13,7 @@ admin.site.register(DHIS2Instance, DHIS2InstanceAdmin)
 class OrgUnitAdmin(admin.ModelAdmin):
     list_display = ("name", "org_unit_id", "parent")
     search_fields = ("name", "org_unit_id", "parent")
-    list_filter = ("instance",)
+    list_filter = ("instance__url",)
 
 
 admin.site.register(OrgUnit, OrgUnitAdmin)
@@ -22,7 +22,7 @@ admin.site.register(OrgUnit, OrgUnitAdmin)
 class DHIS2UsersAdmin(admin.ModelAdmin):
     list_display = ("name", "username", "user_id",)  # "org_units")
     search_fields = ("name", "username", "user_id")
-    list_filter = ("instance",)
+    list_filter = ("instance__url",)
 
     # def org_units(self, instance):
     #     return [ou.name for ou in instance.orgUnits.all()]
@@ -34,7 +34,7 @@ admin.site.register(DHIS2User, DHIS2UsersAdmin)
 class DatasetsAdmin(admin.ModelAdmin):
     list_display = ("name", "dataset_id",)
     search_fields = ("name", "dataset_id")
-    list_filter = ("instance",)
+    list_filter = ("instance__url",)
 
 
 admin.site.register(Dataset, DatasetsAdmin)
