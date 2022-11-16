dfaa297ef107e86a196b4fcc334ec25b63deb2c2
Belendia Serda <belendia@gmail.com>
2022-11-16 10:37:45 +0300

    Add comment describing current state variable

--- flare/apps/dhis/ussd/screen/screen.py
+++ flare/apps/dhis/ussd/screen/screen.py
@@ -24,21 +24,21 @@ class Screen(object):
         self.user_response = user_response
         self.level = level
         self.state = {
-            'passcode': '',
-            'level': Level.LOGIN,
-            'org_unit': '',
-            'period': '',
-            'dataset': '',
-            'period_type': '',
-            'open_future_periods': '',
-            'has_section': '',
-            'section': '',
-            'begin_period': '',
-            'direction': '-',
-            'direction_change': False,
-            'data_element_index': 0,
-            'data_element_values': {},
-            'sections_visited': []
+            'passcode': '', # The password that the user entered to access the DHIS2 form.
+            'level': Level.LOGIN, # The screen that is currently visible to the user.
+            'org_unit': '', # Org unit selected
+            'period': '', # The period in EpiWeek that the user selected.
+            'dataset': '', # The dataset id selected
+            'period_type': '', # The period type i.e. week, month ...
+            'open_future_periods': '',# Shows the number of days, weeks, months... in the future the program should open
+            'has_section': '', # Boolean value to indicate if the selected dataset has section or not
+            'section': '', # The section index that is selected by the user
+            'begin_period': '', # Used in the period screen. The first period to start generating periods.
+            'direction': '-', # Used in the period screen. The default option is - so that users will see past periods.
+            'direction_change': False, # This is used in the period screen. The user had been pressing the - option but had now begun to press the + option.
+            'data_element_index': 0, # The index of the currently displayed data element to the user.
+            'data_element_values': {}, #{data_element_id|category_combo_option_id : value}
+            'sections_visited': [] # Index of the sections that the user visited.
         }
 
         if Store.exists(self.session_id):
