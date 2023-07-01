0477d8cbdcf380c52b7e7e6a09167f0f2658fdd1
Belendia Serda <belendia@gmail.com>
2023-07-01 12:58:51 +0300

    Change the entrypoint.sh and Dockerfile

--- flare/entrypoint.sh
+++ flare/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+APP_PORT = ${PORT:-8000}
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate  --noinput
 python manage.py initadmin
-gunicorn config.wsgi:application --bind 0.0.0.0:8000 --log-level debug
+gunicorn config.wsgi:application --bind "0.0.0.0:${APP_PORT}" --log-level debug
