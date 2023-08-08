3573228b1e2f96a51ba75a2fd894d48afdb825cf
Belendia Serda <belendia@gmail.com>
2023-08-08 14:53:35 +0300

    Refactor the migration code into a shell command

--- flare/entrypoint.sh
+++ flare/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-APP_PORT = ${PORT:-8000}
 python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate  --noinput
-python manage.py initadmin
-gunicorn --workers=3 config.wsgi:application --bind "0.0.0.0:${APP_PORT}" --worker-tmp-dir /dev/shm --log-level info
+
+gunicorn --workers=3 config.wsgi:application --bind "0.0.0.0:8000" --worker-tmp-dir /dev/shm --log-level info
