eed71d6514ac4ab5422c6942dbcce75c2fb67ff0
Belendia Serda <belendia@gmail.com>
2023-09-20 13:43:42 +0300

    Set the Redis host using the env variable

--- flare/config/settings/base.py
+++ flare/config/settings/base.py
@@ -153,7 +153,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
-CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_BROKER_URL = os.getenv('REDIS_URL')  # 'redis://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Africa/Addis_Ababa'
